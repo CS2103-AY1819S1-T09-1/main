b365ff455ce3f546938afb59d080631c1074b27b
John Phua <phuajohn0@gmail.com>
2018-11-02 11:31:48 +0800

    Fix and standardise line breaks in events

--- src/main/java/seedu/address/commons/events/ui/PersonPanelDeselectionEvent.java
+++ src/main/java/seedu/address/commons/events/ui/PersonPanelDeselectionEvent.java
@@ -6,9 +6,9 @@ import seedu.address.commons.events.BaseEvent;
  * Represents a deselection in the Person List Panel
  */
 public class PersonPanelDeselectionEvent extends BaseEvent {
+
     @Override
     public String toString() {
         return getClass().getSimpleName();
     }
-
 }
--- src/main/java/seedu/address/commons/events/ui/TaskPanelDeselectionEvent.java
+++ src/main/java/seedu/address/commons/events/ui/TaskPanelDeselectionEvent.java
@@ -6,9 +6,9 @@ import seedu.address.commons.events.BaseEvent;
  * Represents a deselection in the Task List Panel
  */
 public class TaskPanelDeselectionEvent extends BaseEvent {
+
     @Override
     public String toString() {
         return getClass().getSimpleName();
     }
-
 }
